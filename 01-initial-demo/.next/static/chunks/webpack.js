63aa0e61242b9c12ea0580369c9b92c6a289c1ac
Franco Campaiola <francocampaiola@hotmail.com>
2022-05-28 12:53:34 -0300

    :+1: Ajustes de layout y eliminacion de styles sin usar

--- 01-initial-demo/.next/static/chunks/webpack.js
+++ 01-initial-demo/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a084c0b5541161a4"; }
+/******/ 		__webpack_require__.h = function() { return "661b4479f9f36cde"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
