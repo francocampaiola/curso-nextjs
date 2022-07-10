8912c8f409d02c2e7cb511ff052e5c9da9394dd9
Franco Campaiola <francocampaiola@hotmail.com>
2022-07-10 20:32:59 -0300

    :+1: Se completa la tarea 1 del curso

--- 01-initial-demo/.next/static/chunks/webpack.js
+++ 01-initial-demo/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bda713f7a30f7870"; }
+/******/ 		__webpack_require__.h = function() { return "3b8632b987e35ac3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ 01-initial-demo/pages/pricing/index.js
@@ -0,0 +1,7 @@
+import { MainLayout } from '../../components/layouts/MainLayout'
+
+export default function HomeAbout() {
+  return (
+    <MainLayout pagina="Pricing" href="/" retorno="Home"/>
+  )
+}
